4bd5d4cc2cea2605f3ae86722e12c2a34976eb02
k-reh <1468341475@qq.com>
2019-12-02 18:06:11 +0800

    1202

---
+++ kcartoon_web/src/main/java/com/kreh/kcartoon/KcartoonApplication.java
@@ -0,0 +1,13 @@
+package com.kreh.kcartoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KcartoonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KcartoonApplication.class, args);
+    }
+
+}
---
+++ kcartoon_web/src/test/java/com/kreh/kcartoon/KcartoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kreh.kcartoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KcartoonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
